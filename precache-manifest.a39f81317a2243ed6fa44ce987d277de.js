71bf415c7ad9ab7c70bd3707c83b7c8995394dae
worldmaster <glaubercold53@gmail.com>
2020-02-25 22:44:28 -0300

    Updates

---
+++ precache-manifest.a39f81317a2243ed6fa44ce987d277de.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d756e6a85002fba5ce04423cebfca45c",
+    "url": "/VI-Enc-do-Semiarido/index.html"
+  },
+  {
+    "revision": "d3351021c282c042188f",
+    "url": "/VI-Enc-do-Semiarido/static/css/main.50eca23d.chunk.css"
+  },
+  {
+    "revision": "dcd8aa88a281671225a5",
+    "url": "/VI-Enc-do-Semiarido/static/js/2.283034d3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/VI-Enc-do-Semiarido/static/js/2.283034d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3351021c282c042188f",
+    "url": "/VI-Enc-do-Semiarido/static/js/main.c627911c.chunk.js"
+  },
+  {
+    "revision": "ebdd64cc820d3acfbac5",
+    "url": "/VI-Enc-do-Semiarido/static/js/runtime-main.99955167.js"
+  },
+  {
+    "revision": "a2331882fc700c1a139e470c84336ee7",
+    "url": "/VI-Enc-do-Semiarido/static/media/02.a2331882.jpg"
+  },
+  {
+    "revision": "56f6e96fa70cb6a4feae2227a515b0b7",
+    "url": "/VI-Enc-do-Semiarido/static/media/campina.56f6e96f.jpg"
+  },
+  {
+    "revision": "e82d2f1607fe27656ccbc51c9b6a97d3",
+    "url": "/VI-Enc-do-Semiarido/static/media/campina02.e82d2f16.jpg"
+  },
+  {
+    "revision": "8176eee2101bf7086a146d0717ea4846",
+    "url": "/VI-Enc-do-Semiarido/static/media/cid1.8176eee2.jpg"
+  },
+  {
+    "revision": "8f2616c2a2bb904c3beb420fb8d37857",
+    "url": "/VI-Enc-do-Semiarido/static/media/imgsatcamp1.8f2616c2.png"
+  },
+  {
+    "revision": "21c3e8f7ffd889ad0d550037b20344da",
+    "url": "/VI-Enc-do-Semiarido/static/media/ins1.21c3e8f7.png"
+  },
+  {
+    "revision": "cc12b2db38027af5a3d1060ad92ed511",
+    "url": "/VI-Enc-do-Semiarido/static/media/logo.cc12b2db.png"
+  }
+]);
